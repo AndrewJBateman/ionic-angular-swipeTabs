4438add398c274da95f4b221e8167fcb8fc1bbbd
Andrew Bateman <gomezbateman@yahoo.com>
2020-01-28 12:52:58 +0100

    tutorial completed and working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { NewsPage } from '../news/news.page';
 import { AccountPage } from '../account/account.page';
 import { AboutPage } from '../about/about.page';
@@ -9,7 +9,7 @@ import { IonNav } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
 
   newsPage = NewsPage;
   accountPage = AccountPage;
@@ -21,5 +21,30 @@ export class HomePage {
   @ViewChild('newsNav', {static: false}) newsNav:IonNav;
   
   constructor() {}
+
+  ngAfterViewInit() {
+    this.newsNav.ionNavDidChange.subscribe(async ev => {
+      console.log('Change: ', ev)
+      this.canGoBack = await this.newsNav.canGoBack();
+    });
+  }
+
+  goBack() {
+    this.newsNav.pop();
+  }
+
+  onTabChange(ev: any) {
+    switch (ev.detail.index) {
+      case 0:
+        this.title = 'News';
+        break;
+      case 1:
+        this.title = 'Account';
+        break;
+      case 2:
+        this.title = 'About';
+        break;
+    }
+  }
 
 }
